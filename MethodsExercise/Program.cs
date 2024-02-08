2fe62cc0600bc32363429215d5fd512fe1f0040c
Darren Dhanpat <darren.dhanpat@yahoo.com>
2024-02-07 23:20:48 -0500

    completed methods exercise part 2

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,7 +1,56 @@
-﻿namespace MethodsExercise
+﻿using System.Xml.Schema;
+
+namespace MethodsExercise
 {
     public class Program
     {
+        //methods to add, subtract, multiply, divide
+        //Using the params functionality to allow multiple values inserted into an array
+        public static int Add(params int[] numbers) 
+        {   
+            //declare total variable
+            int total = 0;
+            //loop through numbers array
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                //add up all the number in the numbers array
+                total += numbers[i];
+                
+            }
+            //return the total
+            return total;
+        }
+
+        //Subtraction
+        public static int Subtract(int num1, int num2)
+        {
+            //declare a difference variable
+            int difference = 0;
+
+            //return the difference
+            return difference = num1 - num2;
+        }
+
+        //Multiplication
+        public static int Multiply(int num1, int num2)
+        {
+            //declare a product variable
+            int product = 0;
+
+            //return the product
+            return product = num1 * num2;
+        }
+
+        //Division
+        public static int Divide(int num1, int num2)
+        {
+            //declare a quotient variable
+            int quotient = 0;
+
+            //return the quotient
+            return quotient = num1 / num2;
+        }
+
         static void Main(string[] args)
         {
             //Ask the user for things like a name, color, an animal, etc.
@@ -23,7 +72,21 @@
                 $" which is: {favoriteColor}.\nYou've always wanted to star in {favoriteMovie}, " +
                 $"but ended up being a software developer.");
 
-           
+            Console.WriteLine("Exercise 2");
+            //-----------------------------------------------------------------
+
+            //Addition
+            Console.WriteLine($"The Total is: " + Add(1,2,4,7,3));
+
+            //Subtract
+            Console.WriteLine($"The Difference is: " + Subtract(5,4));
+
+            //Multiply
+            Console.WriteLine($"The Product is: " + Multiply(5, 4));
+
+            //Divide
+            Console.WriteLine($"The Quotient is: " + Divide(10, 5));
+
 
 
             Console.ReadLine();
