5d42dbe04c64765069b4d4726ac98405c7348640
Darren Dhanpat <darren.dhanpat@yahoo.com>
2024-02-07 21:26:12 -0500

    Completed exercise 1

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Ask the user for things like a name, color, an animal, etc.
+            //Make up a story and be creative.
+            Console.WriteLine("What is your name?");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            string favoriteColor = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite food?");
+            string favoriteFood = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite movie?");
+            string favoriteMovie = Console.ReadLine();
+
+            Console.WriteLine($"Your name is {userName} and your favorite food to eat is " +
+                $"{favoriteFood}.\nWhen you are sleeping you dream of your favorite color" +
+                $" which is: {favoriteColor}.\nYou've always wanted to star in {favoriteMovie}, " +
+                $"but ended up being a software developer.");
+
+           
+
+
+            Console.ReadLine();
         }
     }
 }
